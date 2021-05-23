eb6cec31213e5230c0cf588c8a0e9fd63c65c398
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-05-23 19:27:22 +0100

    added experience source which is responsible from collecting the history from the agent

---
+++ common/experience.py
@@ -0,0 +1,153 @@
+from .agent import BaseAgent
+from .common import utils
+
+# one single experience step
+Experience = namedtuple('Experience', ['state', 'action', 'reward', 'done'])
+
+
+def _group_list(items, lens):
+    """
+    Unflat the list of items by lens
+    :param items: list of items
+    :param lens: list of integers
+    :return: list of list of items grouped by lengths
+    """
+    res = []
+    cur_ofs = 0
+    for g_len in lens:
+        res.append(items[cur_ofs:cur_ofs+g_len])
+        cur_ofs += g_len
+    return res
+
+
+class ExperienceSource:
+    """
+    Simple n-step experience source using single or multiple environments
+    Every experience contains n list of Experience entries
+    """
+    def __init__(self, env, agent, steps_count=2, steps_delta=1, vectorized=False):
+        """
+        Create simple experience source
+        :param env: environment or list of environments to be used
+        :param agent: callable to convert batch of states into actions to take
+        :param steps_count: count of steps to track for every experience chain
+        :param steps_delta: how many steps to do between experience items
+        :param vectorized: support of vectorized envs from OpenAI universe
+        """
+        assert isinstance(env, (gym.Env, list, tuple))
+        assert isinstance(agent, BaseAgent)
+        assert isinstance(steps_count, int)
+        assert steps_count >= 1
+        assert isinstance(vectorized, bool)
+        if isinstance(env, (list, tuple)):
+            self.pool = env
+        else:
+            self.pool = [env]
+        self.agent = agent
+        self.steps_count = steps_count
+        self.steps_delta = steps_delta
+        self.total_rewards = []
+        self.total_steps = []
+        self.vectorized = vectorized
+
+    def pop_total_rewards(self):
+        r = self.total_rewards
+        if r:
+            self.total_rewards = []
+            self.total_steps = []
+        return r
+
+    def pop_rewards_steps(self):
+        res = list(zip(self.total_rewards, self.total_steps))
+        if res:
+            self.total_rewards, self.total_steps = [], []
+        return res
+
+    def __iter__(self):
+        states, agent_states, histories, cur_rewards, cur_steps = [], [], [], [], []
+        env_lens = []
+        for env in self.pool:
+            obs = env.reset()
+
+            if self.vectorized:
+                obs_len = len(obs)
+                states.extend(obs)
+            else:
+                obs_len = 1
+                states.append(obs)
+            env_lens.append(obs_len)
+            
+            for _ range(obs_len):
+                histories.append(deque(maxlen=self.steps_count))
+                cur_rewards.append(0.0)
+                cur_steps.append(0)
+                agent.states.append(self.agent.initial_state())
+        
+        iter_idx = 0
+        while True:
+            actions = [None] * len(states)
+            states_input = []
+            states_indices = []
+            
+            for idx, state in enumerate(states):
+                if state is None:
+                    actions[idx] = self.pool[0].action_space.sample()
+                else:
+                    states_input.append(state)
+                    states_indices.append(idx)
+
+            if states_input:
+                states_actions, new_agent_states = self.agent(states_input, agent_states)
+                for idx, action in enumerate(states_actions):
+                    g_idx = states_indices[idx]
+                    actions[g_idx] = action
+                    agent_states[g_idx] = new_agent_states[g_idx]
+
+            grouped_actions = _group_list(actions, env_lens)
+            
+            global_ofs = 0
+            for env_idx, (env, action_n) in enumerate(zip(self.pool, grouped_actions)):
+                if self.vectorized:
+                    next_state_n, r_n, is_done_n = env.step(action_n)
+                else:
+                    next_state, r, is_done = env.step(action_n)
+                    next_state_n, r_n, id_done_n = [ext_state], [r], [is_done]
+            
+                for ofs, (action, next_state, r, is_done) in enumerate(zip(action_n, next_state_n, r_n, id_done_n)):
+                    idx = global_ofs + ofs
+                    state = states[idx]
+                    history = histories[idx]
+
+                    cur_rewards[idx] += r
+                    cur_steps[idx] += 1
+
+                    if state is not None:
+                        history.append(Experience(state=state, action=action, reward=r, done=is_done))
+
+                    if len(history) == self.steps_count and iter_idx % self.steps_delta == 0:
+                        yield tuple(history)
+
+                    states[idx] = next_state
+                    
+                    if is_done:
+                        # in case of very short episode (shorter than our steps count), send gathered history
+                        if 0 < len(history) < self.steps_count:
+                            yield tuple(history)
+                        # generate tail of history
+                        while len(history) > 1:
+                            history.popleft()
+                            yield tuple(history)
+                        
+                        self.total_rewards.append(cur_rewards[idx])
+                        self.total_steps.append(cur_steps[idx])
+                        
+                        cur_rewards[idx] = 0.0
+                        cur_steps[idx] = 0
+                        # vectorized envs are reset automatically
+                        states[idx] = env.reset() if not self.vectorized else None
+                        agent_states[idx] = self.agent.initial_state()
+                        history.clear()
+                    
+                global_ofs += len(action_n)
+            iter_idx += 1
+            
