b59933436d2514377ee57e759c48f9e795828673
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-05-25 20:48:46 +0100

    fix environment interaction

--- common/experience.py
+++ common/experience.py
@@ -109,9 +109,9 @@ class ExperienceSource:
             global_ofs = 0
             for env_idx, (env, action_n) in enumerate(zip(self.pool, grouped_actions)):
                 if self.vectorized:
-                    next_state_n, r_n, is_done_n = env.step(action_n)
+                    next_state_n, r_n, is_done_n, _ = env.step(action_n)
                 else:
-                    next_state, r, is_done = env.step(action_n)
+                    next_state, r, is_done, _ = env.step(action_n[0])
                     next_state_n, r_n, id_done_n = [ext_state], [r], [is_done]
             
                 for ofs, (action, next_state, r, is_done) in enumerate(zip(action_n, next_state_n, r_n, id_done_n)):
